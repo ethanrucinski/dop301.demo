990725e0ed247a405f3563e005178e379e3f90e9
dop301 <dop301@2KZ58.local>
2022-07-11 17:46:22 -0400

    cleanup

--- iac/lib/dop301-stack.ts
+++ iac/lib/dop301-stack.ts
@@ -1,7 +1,4 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { DockerImageCode, DockerImageFunction, FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda';
-import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
 
 
