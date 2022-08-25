18ea0aeeb98c17e2f3072a8b97e9556d98b01c25
dop301 <dop301@2KZ58.local>
2022-08-24 20:33:56 -0500

    cleanup

--- iac/lib/dop301-stack.ts
+++ iac/lib/dop301-stack.ts
@@ -1,38 +1,10 @@
-import { DockerImage, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
-import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { DockerImageCode, DockerImageFunction, FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda';
-import { StringParameter } from 'aws-cdk-lib/aws-ssm';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 
 export class Dop301Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const table = new Table(this, 'Table', {
-      partitionKey: {
-        type: AttributeType.STRING,
-        name: 'UserName'
-      }
-    })
-
-    new StringParameter(this, 'TableName', {
-      parameterName: 'TableName',
-      stringValue: table.tableName
-    })
-
-    const getUsersfunction = new DockerImageFunction(this, 'GetUsersFunction', {
-      code: DockerImageCode.fromImageAsset("../get-users"),
-      environment: {
-        TABLE_NAME: table.tableName
-      }
-    })
-
-    table.grantReadData(getUsersfunction);
-
-    getUsersfunction.addFunctionUrl({
-      authType: FunctionUrlAuthType.NONE
-    })
 
   }
 }
